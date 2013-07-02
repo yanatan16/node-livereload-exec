b641d92d38bf42898a3d4776c6d5895b0a8325aa
Jon Eisen <jeisen@underarmour.com>
2013-07-02 12:41:49 -0400

    Made it watch . if no args

--- livereload-exec.js
+++ livereload-exec.js
@@ -1,12 +1,19 @@
 var livereload = require('livereload'),
 	server = livereload.createServer();
 
-console.log('Started livereload server.')
-
-process.argv.slice(2).forEach(function (d) {
+function watch(d) {
 	if (d[0] !== '/') {
 		d = process.cwd() + '/' + d;
 	}
+
 	server.watch(d);
 	console.log('Watching ' + d);
-});
+}
+
+console.log('Started livereload server.');
+
+if (process.argv.length > 2) {
+	process.argv.slice(2).forEach(watch);
+} else {
+	watch('.');
+}
